624676c992cce9c5f1de32f314e434a0b4cfcb80
Waleed Arif <waleedarifsiddiqui@gmail.com>
2025-01-22 16:42:38 +0500

    finish

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Story from "./components/Story";
+import Features from "./Features";
+
+const App = () => {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar />
+      <Hero />
+      <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
+    </main>
+  );
+};
+
+export default App;
